76f5b8bb48f08a50021aeb17f7c2b2c6fcdb5d4b
Basil Okache <basilokache@gmail.com>
2025-04-26 16:48:39 +0100

    nCr recursion

---
+++ cncpp/recursion/ncr.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int fact(int n)
+{
+    if (n == 0){
+        return 1;
+    }
+    else {
+        return fact(n-1)*n;
+    }
+}
+
+int nCr(int n, int r)
+{
+    int num, den;
+
+    num = fact(n);
+    den = fact(r) * fact(n-r);
+    return num/den;
+}
+
+int main(void)
+{
+    printf("%d\n", nCr(5, 2));
+    return 0;
+}
