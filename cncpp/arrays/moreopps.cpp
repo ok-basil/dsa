cb240060acc248864760bba9a44b2815a2d28880
Basil Okache <basilokache@gmail.com>
2025-05-24 17:07:17 +0100

    Updating operations on array

--- cncpp/arrays/moreopps.cpp
+++ cncpp/arrays/moreopps.cpp
@@ -143,6 +143,21 @@ int Average(struct Array arr)
     return (float) sum/arr.length;
 }
 
+void InsertSort(struct Array *arr, int x)
+{
+    if (arr->length == arr->size)
+        return;
+    
+    int i = arr->length - 1;
+    while (i >=0 && arr->A[i] > x)
+    {
+        arr->A[i + 1] = arr->A[i];
+        i--;
+    }
+    arr->A[i+1] = x;
+    arr->length++;
+}
+
 void DisplayArr(struct Array arr)
 {
     int i = 0;
