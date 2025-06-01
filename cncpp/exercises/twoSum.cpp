f1e68446b222d880fb1eb72d0ec1d8853c212863
Basil Okache <basilokache@gmail.com>
2025-06-01 13:00:11 +0100

    two Sum exercise

---
+++ cncpp/exercises/twoSum.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int* twoSum (int* nums, int numsSize, int target, int* returnSize)
+{
+    static int result[2];
+    for (int i = 0; i < numsSize; i++)
+    {
+        for (int j = i + 1; j < numsSize; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                result[0] = i;
+                result[1] = j;
+                *returnSize = 2;
+                return result;
+            }
+        }
+    }
+    *returnSize = 0;
+    return NULL;
+}
+int main()
+{
+    int nums[] = {2, 7, 11, 15};
+    int target = 9;
+    int returnSize;
+    int* indices = twoSum(nums, 4, target, &returnSize);
+
+    if (indices != NULL) {
+        printf("Indices: %d, %d\n", indices[0], indices[1]);
+    } else {
+        printf("No two numbers add up to the target.\n");
+    }
+    return 0;
+}
