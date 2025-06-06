97825124de98ee9394b8d861c8b90439af848a4c
Basil Okache <basilokache@gmail.com>
2025-06-06 22:01:24 +0100

    Updating array methods

--- cncpp/arrays/allarrayopps.cpp
+++ cncpp/arrays/allarrayopps.cpp
@@ -398,5 +398,58 @@ struct Array * Difference(struct Array *arr1, struct Array *arr2)
 
 int main(void)
 {
-    
+    struct Array arr1;
+    int ch;
+    int x, index;
+
+    printf("Enter size of array: ");
+    scanf("%d", &arr1.size);
+    arr1.A = (int *)malloc(arr1.size * sizeof(int));
+    arr1.length = 0;
+
+    do
+    {
+        printf("\n\nMenu\n");
+        printf("1. Insert\n");
+        printf("2. Delete\n");
+        printf("3. Search\n");
+        printf("4. Sum\n");
+        printf("5. Display\n");
+        printf("6. Exit\n");
+        
+
+        printf("Enter your choice: ");
+        scanf("%d", &ch);
+
+        switch (ch)
+        {
+            case 1:
+                printf("Enter an element and index: ");
+                scanf("%d %d", &x, &index);
+                Insert(&arr1, index, x);
+                break;
+            case 2:
+                printf("Enter index: ");
+                scanf("%d", &index);
+                x = Delete(&arr1, index);
+                printf("Deleted element is %d\n", x);
+                break;
+            case 3:
+                printf("Enter element to search: ");
+                scanf("%d", &x);
+                index = LinearSearch(arr1, x);
+                printf("Element index %d\n", index);
+                break;
+            case 4:
+                printf("Sum is %d\n", Sum(arr1));
+                break;
+            case 5:
+                Display(arr1);
+                break;
+            case 6:
+                return 0;
+        }
+    } while (ch < 6);
+
+    return 0;
 }
