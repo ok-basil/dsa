d0064a3948487b0da1d91388f36ada7765003948
Basil Okache <basilokache@gmail.com>
2025-07-15 20:59:46 +0100

    Reverse

--- cncpp/strings/reverse.cpp
+++ cncpp/strings/reverse.cpp
@@ -2,13 +2,18 @@
 using namespace std;
 
 void reverse(char A[]);
+void reverse2(char B[]);
+
 int main() {
     char A[] = "Python";
+    char B[] = "Alexander";
     
-    reverse(A);
+    // reverse(A);
+    reverse2(B);
     return 0;
 }
 
+// Reverse copy a string using another array
 void reverse(char A[])
 {
     int i;
@@ -24,5 +29,26 @@ void reverse(char A[])
     {
         B[j] = A[i];
     }
+    cout << "Reversed string is " << B << endl;
+}
+
+// Reversing a string with the same array
+void reverse2(char B[])
+{
+    int i, j;
+
+    for (j = 0; B[j] != '\0'; j++)
+    {
+
+    }
+    j = j - 1;
+    for (i = 0; i < j; i++, j--)
+    {
+        int temp;
+        temp = B[i];
+        B[i] = B[j];
+        B[j] = temp;
+    }
+
     cout << "Reversed string is " << B << endl;
 }
